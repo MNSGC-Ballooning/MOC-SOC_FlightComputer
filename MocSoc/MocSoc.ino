b4e2183f294ec235c48810b51dca52bb760c982b
PaulWehling <49325504+PaulWehling@users.noreply.github.com>
2020-08-10 02:20:57 -0500

    Recode to include flight changes

--- MocSoc/MocSoc.ino
+++ MocSoc/MocSoc.ino
@@ -1,8 +1,8 @@
 //MOC-SOC Flight Computer
 //Written by Paul Wehling with assistance from Lilia Bouayed, Based on code originally written by Jordan Bartlett and Emma Krieg.
-//For use with MOC-SOC 3U Cubesat 1.1, built by Paul Wehling and Donald Rowell, based on original by Jordan Bartlett and Emma Krieg.
+//For use with MOC-SOC 3U Cubesat 1.2, built by Paul Wehling and Donald Rowell, based on original by Jordan Bartlett and Emma Krieg.
 //Current code built to run alongside ParasitePCB code on PPOD Deployment System.
-//Last updated June 19, 2020
+//Last updated August 10, 2020
 
   #include <Servo.h>
   #include <SD.h>
@@ -11,16 +11,21 @@
   #include <Adafruit_INA219.h>
   #include <GeigerCounter.h>
   #include <RelayXBee.h>
-  #include <UbloxGPS.h>
+  #include <UbloxGPS.h>       //MNSGC "FlightGPS" library
 
   #define chipSelect 4        //primary pin for SD Logging
-  #define tempBus 11          //data pin for temperature probe
+  #define tempBus 10          //data pin for temperature probe
   #define smartPin 6          //digital pin for SMART release system
   #define rbfPin A0           //pin through which remove-before-flight pin connects to 5V rail
-  #define radioConnectPin A1  //pin that monitors radio connection status
-  #define timeTillDeploy 70   //flight time in minutes after which to deploy panels
+  #define radio_conn_pin A1   //pin that monitiers radio's connection status
+  #define timeTillDeploy 75   //flight time in minutes after which to deploy panels
+  #define northFence 44.63    //north gps fence for deployment, -200 to deactivate
+  #define eastFence -92.52    //east gps fence for deployment, -200 to deactivate
+  #define southFence 43.50    //south gps fence for deployment, -200 to deactivate
+  #define westFence -95.75    //west gps fence for deployment, -200 to deactivate
+  #define altFence 75000      //altitude (in ft) for deployment, -200 to deactivate
   #define xbeeSerial Serial   //communication channel for shield XBee
-  #define cycle_time_sec 10   //time in seconds for a complete logging cycle
+  #define cycle_time_sec 5    //time in seconds for a complete logging cycle
   #define ubloxSerial Serial1 //communication channel for UBLOX GPS
   #define gpsTolerance 25     //ft/s the gps is allowed to drift from last contact
   #undef  abs                 //allows abs() to be used as a regular function instead of the macro that Arduino usually uses
@@ -54,9 +59,21 @@
   String report_data;                                             //contains qualitative data and radio traffic receipts
   bool extra_data = false;                                        //tracks if there is extra data to print during a given cycle
   bool ppod_deploy_ping = false;                                  //tracks if PPOD has deployed, allows MOC-SOC deployment after recieved ping. Ignored by 'DEPLOY' command
-  int last_data_relayed = 0;                                      //keeps note of what data has and has not been sent via telemetry link
-  int last_report_relayed = 0;                                    //keeps note of what reports have and have not been sent via telemetry link
-  bool smart_telemetry_enabled = true;                            //turns 'smart telemetry' protocol on and off, if false, all data is relayed as it is gathered
+  bool radio_tick = false;                                        //tracks if radio has made a connection yet during cycle
+  bool smart_telemetry = false;                                   //tracks if advanced telemetry is enabled, flips if SD card fails during bootup
+  unsigned long last_report_relayed = 0;                          //tracks last sent report file line
+  unsigned long last_data_relayed = 0;                            //tracks last sent data file line
+  float n_fence = northFence;                                     //gps fence value for solar arm deployment, if -200 fence deactivated
+  float e_fence = eastFence;                                      //gps fence value for solar arm deployment, if -200 fence deactivated
+  float s_fence = southFence;                                     //gps fence value for solar arm deployment, if -200 fence deactivated
+  float w_fence = westFence;                                      //gps fence value for solar arm deployment, if -200 fence deactivated
+  float a_fence = altFence;                                       //altitude fence value for solar arm deployment, if -200 fence deactived
+  bool dply_geo_fence = true;                                     //toggles gps (NESW) fence usage, if true gps fences can trigger deployment
+  bool dply_alt_fence = true;                                     //toggles altitude fence usgae, if true altitude fence can trigger deployment
+  bool dply_timer = true;                                         //toggles timer usage, if true flight timer can trigger deployment
+  bool dply_descent = true;                                       //toggles automatic deploy after detecting burst, if true any descent triggers deployment
+  bool force_ppod_deploy = true;                                  //toggles automatic backup PPOD deploy, if true PPOD will recive deploy command when MOC-SOC attempts deployment
+  unsigned int tracked_millis = 0;                                //holds last millis() value betweeen cycles for data clarity check
   
 
 void setup() {
@@ -73,13 +90,11 @@ void loop() {
   else if(!smart_release_attempt) checkDeploy();                  //activates SMART after cutTime elapses
   recieveCommands();                                              //checks XBee and freewave for commands from ground/comms/etc
   attemptGPS(false);                                              //tries to establish a gps lock and filters for good and bad hits
-  printdata();                                                    //prints the data to serial, SD, and XBEE
-  if(smart_telemetry_enabled) radio_check();                      //attempts connection to ground station, if established downlinks all data since last connection
+  printdata();                                                    //prints the data to radio, serial, SD, and XBEE
   wait();                                                         //holds until cycle time has elapsed
 }
 
 void sdSetup() {                 //starts SD Card logging and creates logging files
-  //pinMode(53,OUTPUT);
   printout("Initializing SD card",true);
   if(!SD.begin(chipSelect)) printout("ERROR: Card failed, or not present",true);
   else {
@@ -91,8 +106,6 @@ void sdSetup() {                 //starts SD Card logging and creates logging fi
         datalog = SD.open(report_filename, FILE_WRITE);
         datalog.close();
         SD_report_active = true;
-        printout(" ",true);  //loads blank line into report so radio_check doesn't miss it
-        printout(" ",true,true);  //same thing with data
         printout("Logging report to: " + String(report_filename),true);
         break;
       }
@@ -110,50 +123,40 @@ void sdSetup() {                 //starts SD Card logging and creates logging fi
     }
     if (!SD_report_active) printout("ERROR: No available report file names, clear SD card to enable logging",true);
     if (!SD_data_active) printout("ERROR: No available data file names, clear SD card to enable logging",true);
-    if (!SD_data_active || !SD_report_active) {
-      smart_telemetry_enabled = false;
-      printout("WARNING: Smart telemetry disabled",true);
-    }
   }
+  if (SD_report_active && SD_data_active) smart_telemetry = true;
+  else printout("WARNING: Smart telemetry disabled", true);
 }
 
 void printout(String to_print, bool endline) {
   printout(to_print,endline,false);
 }
 
-void printout(String to_print, bool endline, bool data) {    //prints to moniter(XBEE), endline is for print (false) or println (true), data tells what file to write to
-  Serial.print(to_print);
-  if(endline) {
-    Serial.println();
-   if(!smart_telemetry_enabled) {
-    Serial2.print("$M$");
-    Serial2.print(to_print);
-    if(endline) {
-      Serial2.println();
-    }
-   }
-  }
+void printout(String to_print, bool endline, bool data) {    //prints to both moniter(XBEE) and radio, endline is for print (false) or println (true), data tells what file to write to
+  Serial.print("$M$" + to_print);
+  if (endline) Serial.println();
   if(!(data) && SD_report_active) {
     report_data += to_print;
-    if(endline) report_data += "\n";
     extra_data = true;
     }
+  if(!smart_telemetry) {
+    Serial2.print("$M$" + to_print);
+    if(endline) Serial2.println();
+  }
 }
 
 void printdata() {
   sensors.requestTemperatures();
   String data = gps_lock + String(sensors.getTempCByIndex(0)) + "," + String(ina219.getBusVoltage_V()) + "," + String(ina219.getShuntVoltage_mV()/1000.0) + "," 
   + String(ina219.getCurrent_mA()) + "," + String(ina219.getPower_mW()) + "," + String(geiger1.getTotalCount()) + "," + String(geiger1.getCycleCount()) + ","
-  + String(ppod_deploy_ping);
+  + String(ppod_deploy_ping) + "," + String(tracked_millis) + ",";
+  tracked_millis = millis();
+  data += String(tracked_millis) + "," + String(tracked_millis);
   printout(data,true,true);
+  if(extra_data) data += "," + report_data;
   if(SD_data_active) {
     datalog = SD.open(data_filename, FILE_WRITE);
     datalog.println(data);
-    datalog.close();
-  }
-  if(extra_data && SD_report_active) {
-    datalog = SD.open(report_filename, FILE_WRITE);
-    datalog.print(report_data);
     datalog.close();
     extra_data = false;
     report_data = "";
@@ -189,11 +192,30 @@ void checkStart() {
 }
 
 void checkDeploy()  {
-  if(((millis()- millis_start_time) > (cutTime*60000)) && ppod_deploy_ping)  {
-    servo.write(180);
-    smart_release_attempt = true;
-    printout("SMART triggered at " + String(millis()),true);
+  if(dply_timer && ((millis()- millis_start_time) > (cutTime*60000)))  attemptDeploy();
+  if((dply_geo_fence || dply_alt_fence) && gps_good)  {
+    String temp_coords = gps_lock;
+    float temp_calc = 0;
+    for(int i=0; i<2; i++)  {
+      temp_coords.remove(0,temp_coords.indexOf(','));
+      temp_calc = temp_coords.substring(0,temp_coords.indexOf(',')).toFloat();
+      if(dply_geo_fence && (((s_fence != -200) && (temp_calc < s_fence)) || ((n_fence != -200) && (temp_calc > n_fence)))) attemptDeploy();
+      temp_coords.remove(0,temp_coords.indexOf(','));
+      temp_calc = temp_coords.substring(0,temp_coords.indexOf(',')).toFloat();
+      if(dply_geo_fence && (((w_fence != -200) && (temp_calc < w_fence)) || ((e_fence != -200) && (temp_calc > e_fence)))) attemptDeploy();
+      temp_coords.remove(0,temp_coords.indexOf(','));
+      temp_calc = temp_coords.substring(0,temp_coords.indexOf(',')).toFloat();
+      if(dply_alt_fence && (temp_calc > a_fence)) attemptDeploy();
+    }
   }
+  if(dply_descent && gps_good && (gps_alt[0] < gps_alt[1]) && (gps_alt[1] < gps_alt[2]))  attemptDeploy();
+}
+
+void attemptDeploy()  {
+  servo.write(180);
+  smart_release_attempt = true;
+  printout("SMART triggered ar " + String(millis()),true);
+  if(force_ppod_deploy) Serial.println("PPOD?CUT!");
 }
 
 void attemptGPS(bool override_gps) {
@@ -208,7 +230,7 @@ void attemptGPS(bool override_gps) {
     gps.update();
     gps_alt_current = gps.getAlt_meters();
     for(byte i=0; i<3; i++) {
-      if(!((abs(gps_alt_current - gps_alt[i])) < (gpsTolerance * 1000 * (millis() - (millis_start_time + gps_time[i]))))) {
+      if(!((abs(gps_alt_current - gps_alt[i])) < (gpsTolerance * 1000 * (millis() - gps_time[i])))) {
         override_gps = true;
       }
     }
@@ -228,18 +250,25 @@ void attemptGPS(bool override_gps) {
                   + String(gps.getHour()) + ":" + String(gps.getMinute()) + ":" + String(gps.getSecond()) + ","
                   + String(gps.getLat(), 4) + "," + String(gps.getLon(), 4) + "," + String(gps_alt_current, 1) + ","
                   + String(gps.getSats()) + ",";
-  if(gps.getFixAge() > cycle_time * 2) gps_lock += "No Fix,";
+  if(gps.getFixAge() > cycle_time * 2) {
+    gps_lock += "No Fix,";
+    gps_good = false;
+  }
   else gps_lock += "Fix,";
+
+  if(gps_good) gps_lock += "GPSCLR,";
+  else gps_lock += "GPSBAD,";
 }
 
 void wait() {
   while(millis()-last_millis_overflow < cycle_time){
     gps.update();
     recieveCommands();
-    radio_check();
+    if(!radio_tick && smart_telemetry) radioConnect();
     delay(10);
   }
   last_millis_overflow = millis();
+  radio_tick = false;
 }
 
 void recieveCommands()  {
@@ -324,16 +353,6 @@ void commandRegister(String command)  {
       printout("Ready to deploy with PPOD authorization",true);
     }
   }
-  else if(command.equals("TOGGLETELEMETRYMODE"))  {               //Enables or disables 'smart telemetry', where radio sends packets only when connected
-    smart_telemetry_enabled = !smart_telemetry_enabled;
-    if (smart_telemetry_enabled) printout("Smart telemetry protocol enabled\nWARNING: data may be lost if SD logging has failed",true);
-    else printout("Smart telemetry protocol disabled\nWARNING: data packets may be lost if radio connection is lost",true);
-  }
-  else if(command.equals("RESENDALL")) {                             //Rapidly sends all data taken so far in flight
-    last_data_relayed=0;
-    last_report_relayed=0;
-    radio_check();
-  }
   else if(command.equals("START"))  {                             //Activates measurement and sets start time if not already started
     printout("Flight started via command at ",false);
     if(!(flight_begun)) {
@@ -349,15 +368,37 @@ void commandRegister(String command)  {
     resetGPS();
     printout("GPS reset attempt completed",true);
   }
-  else if ((command.substring(0,14)).equals("RESENDLASTDATA"))  {     //Resends inputted value of last data, ie 'RESENDLASTDATA30' would send the 30 most recent data lines
-    command.remove(0,14);
-    last_data_relayed -= command.toFloat();
-    radio_check;
+  else if(command.equals("TOGGLERADIOMODE"))  {                   //Switches smart telemetry on or off
+    smart_telemetry = !smart_telemetry;
+    if(smart_telemetry) printout("WARNING: Smart telemetry enabled. Data and report relay may be lost if SD card error",true);
+    else printout("WARNING: Smart telemetry disabled. Data and report relay may be lost if connection error",true);
   }
-  else if ((command.substring(0,17)).equals("RESENDLASTREPORTS"))  {     //Resends inputted value of last reports, ie 'RESENDLASTREPORTS30' would send the 30 most recent data lines
-    command.remove(0,17);
-    last_data_relayed -= command.toFloat();
-    radio_check;
+  else if((command.substring(0,5)).equals("FENCE"))  {             //Allows changing of gps and altitude fences, i.e. 'FENCEN-200' would set the north fence to -200, or off
+    switch(command.charAt(5)) {
+      case('N'): n_fence = (command.substring(6)).toFloat();
+      case('E'): e_fence = (command.substring(6)).toFloat();
+      case('S'): s_fence = (command.substring(6)).toFloat();
+      case('W'): w_fence = (command.substring(6)).toFloat();
+      case('A'): a_fence = (command.substring(6)).toFloat();
+      case(26):;
+      default: printout("WARNING: Fence command not recognized",true);
+    }
+    printout("Fences set to " + String(n_fence) + "N, " + String(s_fence) + "S, " + String(e_fence) 
+      + "E, " + String(w_fence) + "W, " + String(a_fence) + "alt(ft)",true);
+  }
+  else if((command.substring(0,9)).equals("SETDEPLOY"))  {         //Toggles deployment booleans, G for GPS, A for altitude, T for timer, D for descent, P for forced PPOD deploy
+    switch(command.charAt(9)) {
+      case('G'): dply_geo_fence = !dply_geo_fence;
+      case('A'): dply_alt_fence = !dply_alt_fence;
+      case('T'): dply_timer = !dply_timer;
+      case('D'): dply_descent = !dply_descent;
+      case('P'): force_ppod_deploy = !force_ppod_deploy;
+      case(26):;
+      default: printout("WARNING: Deployment command not recognized",true);
+    }
+    printout("Deployment bools set to " + String(dply_geo_fence) + "G " + String(dply_alt_fence) 
+      + "A " + String(dply_timer) + "T " + String(dply_descent) + "D " + String(force_ppod_deploy) 
+      + "P",true);
   }
   else if((command.substring(0,4)).equals("SETT"))  {             //Changes the launch-till-deploy timer to inputted value, ie 'SETT35' would set timer to 35 minutes from now
     command.remove(0,4);
@@ -384,51 +425,42 @@ void commandRegister(String command)  {
   }
 }
 
-void radio_check() {
-  bool data_done = false;
-  bool report_done = false;
-  bool good_conn = false;
-  char next_ch;
-  if(analogRead(radioConnectPin) > 500) {
-    good_conn = true;
+void radioConnect()  {
+  if(analogRead(radio_conn_pin)>500) {
+    char next_char;
     datalog = SD.open(report_filename, FILE_READ);
-    next_ch=datalog.read();
-    for (int i=0; i<=last_report_relayed; i++) {
-      do next_ch=datalog.read(); while(next_ch != '\n');
+    for(int i = 0; i<=last_report_relayed; i++) {
+      datalog.read(next_char,1);
+      while(next_char != '/n') {
+        datalog.read(next_char,1);
+      }
     }
-    good_conn = (analogRead(radioConnectPin) > 500);
-    if(datalog.peek()==-1) report_done = true;
-    while(good_conn && !report_done) {
-      Serial2.print("$M$");
-      do {
-        next_ch=datalog.read();
-        Serial2.print(next_ch);
-      } while(next_ch != '\n');
-      good_conn = (analogRead(radioConnectPin) > 500);
-      if(good_conn) last_report_relayed++;
-      if(datalog.peek()==-1) report_done = true;
+    while((analogRead(radio_conn_pin)>500) && (datalog.peek() != 26)) {
+      while(next_char != '/n') {
+        Serial2.print(next_char);
+        datalog.read(next_char,1);
+      }
+      if(analogRead(radio_conn_pin)>500) last_report_relayed ++;
     }
     datalog.close();
     
     datalog = SD.open(data_filename, FILE_READ);
-    next_ch=datalog.read();
-    for (int i=0; i<=last_data_relayed; i++) {
-      do next_ch=datalog.read(); while(next_ch != '\n');
+    for(int i = 0; i<=last_data_relayed; i++) {
+      datalog.read(next_char,1);
+      while(next_char != '/n') {
+        datalog.read(next_char,1);
+      }
     }
-    good_conn = (analogRead(radioConnectPin) > 500);
-    if(datalog.peek()==-1) data_done = true;
-    while(good_conn && !data_done) {
-      Serial2.print("$M$");
-      do {
-        next_ch=datalog.read();
-        Serial2.print(next_ch);
-      } while(next_ch != '\n');
-      good_conn = (analogRead(radioConnectPin) > 500);
-      if(good_conn) last_data_relayed++;
-      if(datalog.peek()==-1) data_done = true;
+    while((analogRead(radio_conn_pin)>500) && (datalog.peek() != 26)) {
+      while(next_char != '/n') {
+        Serial2.print(next_char);
+        datalog.read(next_char,1);
+      }
+      if(analogRead(radio_conn_pin)>500) last_data_relayed ++;
     }
     datalog.close();
-    printout(("Connection made at " + String(millis()) + ", data relayed to line " + 
-    String(last_data_relayed) + ", reports to line " + String(last_report_relayed)),true);
+
+    printout("Data relayed to line " + String(last_data_relayed) + ", reports relayed to line " + String(last_report_relayed),true);
+    radio_tick = true;
   }
 }
